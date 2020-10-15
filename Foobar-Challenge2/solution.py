e735a5bec5db8e5acbc675ceb61e678f7fdadd3c
BrendanGlancy <bglance68@gmail.com>
2020-10-15 16:03:40 -0400

    solution.py

--- Foobar-Challenge2/solution.py
+++ Foobar-Challenge2/solution.py
@@ -1,3 +1,11 @@
 def solution(l):
-    result = sorted(l, key=lambda l:[int(i) for i in l.split('.')])
-    print(result)
+    
+    # make a dictionary for the values
+    d = dict.fromkeys(l, 0)
+    # split by the .
+    for v in l:
+        d[v] = list(map(int, v.split('.')))
+    
+    # sort
+    d = sorted(d.items(), key=lambda x: x[1])
+    return [i[0] for i in d]
